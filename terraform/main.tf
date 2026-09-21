5639647f672bbf27b93113fc8d6301b907cd59c7
AnticipatedD <mdabul@cc.cc>
2026-09-21 14:54:57 +0600

    refactor(iac): modularize monolithic terraform configuration into agent_cluster module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,7 @@
 terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
+  required_version = ">= 1.0.0"
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "agent_rg" {
-  name     = "rg-hamdgai-production"
-  location = "East US"
-  
-  tags = {
-    Environment = "Production"
-    ManagedBy   = "Terraform"
-    Project     = "hamdGAI"
-  }
-}
-terraform {
-  required_version = ">= 1.3.0"
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2.0"
-    }
-  }
-}
-
-resource "null_resource" "hamdgai_cluster_stub" {
-  triggers = {
-    agent_version = "1.0.0"
-  }
-}
-
-output "cluster_status" {
-  value = "hamdGAI Infrastructure Module Initialized"
+module "agent_cluster" {
+  source = "./modules/agent_cluster"
 }
---
+++ terraform/modules/agent_cluster/outputs.tf
@@ -0,0 +1,3 @@
+output "cluster_id" {
+  value = null_resource.cluster.id
+}
